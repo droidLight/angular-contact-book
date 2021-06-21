682b987dc0018d60debf847788ae7aa638beb805
droidLight <sibinehru@gmail.com>
2021-06-21 15:42:00 +0530

    added function for checking  if user exists

--- src/app/services/contact/contact.service.ts
+++ src/app/services/contact/contact.service.ts
@@ -7,38 +7,43 @@ import { Subject } from 'rxjs';
 })
 export class ContactService {
 
-  private contactSubject:Subject<any> = new Subject();
+  private contactSubject: Subject<any> = new Subject();
   contactObservable = this.contactSubject.asObservable();
 
-  contacts:any;
-  constructor() { 
+  contacts: any;
+  constructor() {
 
     //getting all contacts
     let keys = Object.keys(localStorage);
     let count = keys.length;
 
     this.contacts = []
-    while(count--){
-      this.contacts.push(this.getContact(keys[count]));      
+    while (count--) {
+      this.contacts.push(this.getContact(keys[count]));
     }
-    
+
   }
 
-  saveContact(data:any){
+  saveContact(data: any) {
     let name = data.contactname;
     localStorage.setItem(name, JSON.stringify(data));
-    
+
     this.contactSubject.next(data);
   }
 
-  getContact(name:string){
+  getContact(name: string) {
     let res = localStorage.getItem(name);
-    let data = (res!==null)?JSON.parse(res):null;
-    
+    let data = (res !== null) ? JSON.parse(res) : null;
+
     return data;
   }
 
-  getAllContacts(){
+  getAllContacts() {
     return [...this.contacts];
+  }
+
+  nameExists(name: string): boolean {
+    let person = localStorage.getItem(name);
+    return (person !== null);
   }
 }
